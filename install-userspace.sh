32b35e67098dd5059728e59b52f805f20aef0ac3
Ross Light <ross@zombiezen.com>
2022-11-14 05:13:20 +0000

    Disable profile modification in the Nix installer

--- install-userspace.sh
+++ install-userspace.sh
@@ -24,7 +24,7 @@ run_as_user() {
 
 # Install Nix. Should be the same version as in userspace.nix.
 echo "Installing Nix..." 1>&2
-run_as_user bash -c 'sh <(curl -fsSL https://releases.nixos.org/nix/nix-2.11.0/install) --daemon'
+run_as_user bash -c 'sh <(curl -fsSL https://releases.nixos.org/nix/nix-2.11.0/install) --daemon --no-modify-profile'
 run_as_root mkdir -p /etc/nix
 echo "extra-trusted-users = $username" | run_as_root tee -a /etc/nix/nix.conf > /dev/null
 
